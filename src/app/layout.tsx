0d77b215b2c99e539561f1e9391ab83ac04ac7e0
JanoTheDev <jeanthaddad@gmail.com>
2025-02-09 17:14:31 +0200

    Add analytics to the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from 'next/script'
 
 export const metadata: Metadata = {
-  title: "Lebanese Driving Test",
-  description: "Official Lebanese Driving Questions Test",
+  title: "Lebanese Driving Test Practice",
+  description: "Practice for your Lebanese driving license computer test",
 };
 
 export default function RootLayout({
@@ -13,6 +14,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="aebfc48a-7794-41ff-a106-ed88beb8900a"
+        />
+      </head>
       <body>
         {children}
       </body>
